16d35ce7f9f59e12938490db7eaba66379171a09
wendantingwang <104487604+wendantingwang@users.noreply.github.com>
2022-06-04 15:13:57 +1000

    commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+var MainImg = document.getElementById ("MainImg")
+var smallimg = document.getElementsByClassName ("small-img")
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src
+
+}
+smallimg[1].onclick =function(){
+    MainImg.src = smallimg[1].src
+
+}
+
+smallimg[2].onclick =function(){
+    MainImg.src = smallimg[2].src
+
+}
+
+smallimg[3].onclick =function(){
+    MainImg.src = smallimg[3].src
+
+}
+
+smallimg[4].onclick =function(){
+    MainImg.src = smallimg[4].src
+
+}
